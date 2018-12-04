afb48148f9a5afcbbe82c1a0121eb1219d0b7243
quangliem <phamquangliem96@gmail.com>
2018-12-04 18:13:58 +0700

    sửa lỗi lặp vô hạn

--- src/main/java/Bayes/NormalDistribution.java
+++ src/main/java/Bayes/NormalDistribution.java
@@ -15,7 +15,7 @@ public class NormalDistribution {
         if (z < -8.0) return 0.0;
         if (z >  8.0) return 1.0;
         double sum = 0.0, term = z;
-        for (int i = 3; Math.abs(term) < 1e-5; i += 2) {
+        for (int i = 3; Math.abs(term) > 1e-9; i += 2) {
             sum  = sum + term;
             term = term * z * z / i;
         }
